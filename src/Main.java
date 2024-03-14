ee8afe63efce25f1e0cc575b4db6722740017361
phongdinta <136043782+phongdinta@users.noreply.github.com>
2024-03-14 08:30:21 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        String inputFilePath = "src/input.txt";
+        String outputFilePath = "src/output.txt";
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
 
-        Bài làm viết tại đây
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilePath));
 
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.replaceAll("Nha Trang", "Vũng Tàu");
+                writer.write(line);
+                writer.newLine();
+            }
+
+            reader.close();
+            writer.close();
+
+            System.out.println("Thay thế và ghi vào file output.txt thành công.");
+        }
+        catch (IOException e) {
+            System.err.println("Xảy ra lỗi khi đọc hoặc ghi file: " + e.getMessage());
+        }
     }
 }
